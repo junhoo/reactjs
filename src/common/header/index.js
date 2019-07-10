bc1d6cac7e8f3bbbe5567e46857e0431bf6c57f8
banzhuanshi <547455277@qq.com>
2019-07-10 22:51:02 +0800

    constants 拆分 / immutable 不可变结构共享

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
+import { actionCreators } from './store';
 import {
 	HeaderWrapper,
 	Logo,
@@ -55,24 +56,18 @@ class Header extends Component {
 
 const mapStateToProps = (state) => {
 	return {
-		focused: state.header.focused
+		focused: state.header.get('focused')
 	}
 }
 
 const mapDispathToProps = (dispatch) => {
   return {
     handleInputFocus() {
-      const action = {
-        type: 'search_focus'
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchFocus())
     },
 
     handleInputBlur() {
-      const action = {
-        type: 'search_blur'
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchBlur())
     }
   }
 }
