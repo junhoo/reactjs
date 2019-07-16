2a7f8ca917dedd2680175648dac9eea82be337d8
banzhuanshi <547455277@qq.com>
2019-07-16 23:26:46 +0800

    route component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header'
+import Home from './pages/home';
+import Detail from './pages/detail';
 import { GlobalStyle } from './statics/iconfont/iconfont';
 import store from './store';
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Header/>
-      <GlobalStyle/>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Header/>
+          <BrowserRouter>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/detail' exact component={Detail}></Route>
+          </BrowserRouter>
+          <GlobalStyle/>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
