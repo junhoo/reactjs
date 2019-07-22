08ac912b460701412e10517c526d3ecdc9b86d8f
banzhuanshi <547455277@qq.com>
2019-07-23 00:31:57 +0800

    loadable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { GlobalStyle } from './statics/iconfont/iconfont';
 import Header from './common/header'
 import Home from './pages/home';
-import Detail from './pages/detail';
+import Detail from './pages/detail/loadable.js';
 import Login from './pages/login';
-import { GlobalStyle } from './statics/iconfont/iconfont';
 import store from './store';
+import Write from './pages/write';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Header/>
             <Route path='/' exact component={Home}></Route>
             <Route path='/login' exact component={Login}></Route>
+            <Route path='/write' exact component={Write}></Route>
             <Route path='/detail/:id' exact component={Detail}></Route>
           </div>
         </BrowserRouter>
