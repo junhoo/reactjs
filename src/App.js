901e58986a06a427ed6388af2d009f08f5158dc0
banzhuanshi <547455277@qq.com>
2019-07-22 21:43:58 +0800

    login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header'
 import Home from './pages/home';
 import Detail from './pages/detail';
+import Login from './pages/login';
 import { GlobalStyle } from './statics/iconfont/iconfont';
 import store from './store';
 
@@ -15,6 +16,7 @@ class App extends Component {
           <div>
             <Header/>
             <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
             <Route path='/detail/:id' exact component={Detail}></Route>
           </div>
         </BrowserRouter>
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import { actionCreators } from './store';
+import { actionCreators as loginActionCreators } from '../../pages/login/store'
 import {
 	HeaderWrapper,
 	Logo,
@@ -60,7 +61,7 @@ class Header extends Component {
   }
 
   render() {
-    const { focused, handleInputFocus, handleInputBlur, list } = this.props;
+    const { focused, handleInputFocus, handleInputBlur, list, login, logout } = this.props;
     return (
       <HeaderWrapper>
         <Link to='/'>
@@ -69,7 +70,11 @@ class Header extends Component {
         <Nav>
           <NavItem className='left action'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
-          <NavItem className='right'>登陆</NavItem>
+					{
+						login ? 
+							<NavItem onClick={logout} className='right'>退出</NavItem> : 
+							<Link to='/login'><NavItem className='right'>登陆</NavItem></Link>
+					}
           <NavItem className='right'>
             <i className="iconfont">&#xe636;</i>
           </NavItem>
@@ -109,7 +114,8 @@ const mapStateToProps = (state) => {
     list: state.getIn(['header', 'list']),
 		page: state.getIn(['header', 'page']),
 		totalPage: state.getIn(['header', 'totalPage']),
-		mouseIn: state.getIn(['header', 'mouseIn']),
+    mouseIn: state.getIn(['header', 'mouseIn']),
+		login: state.getIn(['login', 'login'])
 	}
 }
 
@@ -142,6 +148,9 @@ const mapDispathToProps = (dispatch) => {
 			} else {
 				dispatch(actionCreators.changePage(1));
 			}
+		},
+		logout() {
+			dispatch(loginActionCreators.logout())
 		}
   }
 }
